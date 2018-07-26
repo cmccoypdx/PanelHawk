9a2a74e2d8026b5b0319ac2ece31ae7c13329b96
mick <mccoyc@cat.pdx.edu>
2018-07-26 15:44:42 -0700

    extract pages into local temp/comic folder

--- gui.py
+++ gui.py
@@ -5,25 +5,29 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 
+import unrar
 import rarfile
 import sys
 
 cbr = rarfile.RarFile(sys.argv[1])
-#for f in cbr.infolist():
-#  print (f.filename, f.file_size)
-#  if f.filename == 'README':
-#    print(rf.read(f))
+for f in cbr.infolist():
+  print (f.filename, f.file_size)
+  if f.filename == 'README':
+    print(rf.read(f))
 
-#for fn in cbr.namelist():
-#  print(fn)
+for fn in cbr.namelist():
+  print(fn)
 
-comic = cbr.open(cbr.infolist()[0])
+comic = (cbr.infolist()[5]).filename
 print(type(comic))
+
+cbr.extractall(path='./temp/comic', members=cbr.infolist())
+
 class Gui(App):
 
   def build(self):
-    return Label(text='PanelHawk')
-    #return Image(comic[0])
+    #return Label(text='PanelHawk')
+    return Image(source='/Unnatural 001-006.jpg')
 
 if __name__ == '__main__':
   Gui().run()
