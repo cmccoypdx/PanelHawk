dc0e2fffada460a3e87d628e28a3df4072de931d
mick <mccoyc@cat.pdx.edu>
2018-07-26 16:25:48 -0700

    perform code cleanup and add comments

--- gui.py
+++ gui.py
@@ -12,28 +12,31 @@ import shutil
 import os
 
 cbr = rarfile.RarFile(sys.argv[1])
-for f in cbr.infolist():
-  print (f.filename, f.file_size)
-  if f.filename == 'README':
-    print(rf.read(f))
 
-for fn in cbr.namelist():
-  print(fn)
+# Debug code that prints cbr contents
+#for f in cbr.infolist():
+#  print (f.filename, f.file_size)
+#  if f.filename == 'README':
+#    print(rf.read(f))
 
-comic = (cbr.infolist()[5]).filename
-print(type(comic))
+#for fn in cbr.namelist():
+#  print(fn)
 
-#cbr.extractall(path='./temp/comic', members=cbr.infolist())
+# Extract all pages in cbr to temp folder 'comic'
+cbr.extractall(path='./temp/comic', members=cbr.infolist())
+
+# Build list of page filenames
 pages = os.listdir('./temp/comic')
 
 class Gui(App):
 
   def build(self):
-    #return Label(text='PanelHawk')
-    #print(pages[0])
     return Image(source = './temp/comic/' + pages[0])
-    #return Image(source='./temp/comic/Unnatural001.jpg')
 
 if __name__ == '__main__':
+
+  # Start GUI
   Gui().run()
-  #shutil.rmtree('./temp/comic')
+
+  # Empty temp folder on app exit
+  shutil.rmtree('./temp/comic')
