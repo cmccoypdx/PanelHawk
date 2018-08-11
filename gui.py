e3b2e01ed434152d71857ef8c9cb596534ccaa0c
mick <mccoyc@cat.pdx.edu>
2018-08-11 16:20:47 -0700

    set to open in maximized window, with commented out option for opening fullscreen borderless

--- gui.py
+++ gui.py
@@ -7,6 +7,8 @@ from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 from kivy.graphics import InstructionGroup
 from kivy.core.window import Window
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 
 import rarfile
 import sys
@@ -37,28 +39,41 @@ if len(os.listdir(path)) == 1:
 pages = os.listdir(path)
 pages.sort()
 
+# Best option for mobile
+#Window.fullscreen = 'auto'
 
-class Comic(Widget):
+# Best option for desktop
+Window.maximize()
+
+class Comic(GridLayout):
   pageIndex = 0
 
   # Logic for binding page size to window taken from https://stackoverflow.com/questions/30207707/how-to-fill-canvas-with-an-image-in-kivy
-  def __init__(self, **kwargs):
-    super(Comic, self).__init__(**kwargs)
+  def __init__(self):
+    super(Comic, self).__init__(cols=1)
     with self.canvas:
       self.page = Image(source = path + pages[self.pageIndex], pos=self.pos, size=self.size)
 
+    
+  # Bind page image size relative to size of app window
     self.bind(pos=self.update_bg)
     self.bind(size=self.update_bg)
 
+  # Resize page image when app window is resized
   def update_bg(self, *args):
     self.page.pos = self.pos
     self.page.size = self.size
 
+  # Clear canvas of current page and load page at pageIndex
   def update(self):
     self.canvas.clear()
     with self.canvas:
       self.page = Image(source = path + pages[self.pageIndex], pos=self.pos, size=self.size)
 
+  # Move to next or previous page based on which third of the screen
+  # was touched/clicked. Right third goes to next page, left third
+  # goes to previous page. Middle third or already at furthest navigation
+  # point does nothing.
   def on_touch_down(self, touch):
     if touch.x < self.width / 3:
       if self.pageIndex > 0:
@@ -67,10 +82,6 @@ class Comic(Widget):
       if self.pageIndex < len(pages) - 1:
         self.pageIndex += 1
     self.update()
-
-
-  def build(self):
-    return page
 
 class Gui(App):
 
