1eec0df299892bfe8d5e33667650bc51c3cfbc15
mick <mccoyc@cat.pdx.edu>
2018-08-11 14:38:22 -0700

    bound page size to size of window

--- gui.py
+++ gui.py
@@ -5,6 +5,8 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.core.window import Window
 
 import unrar
 import rarfile
@@ -33,27 +35,36 @@ if len(os.listdir(path)) == 1:
   path += os.listdir(path)[0]
   path += '/'
 pages = os.listdir(path)
-print(pages)
+
 
 class Comic(Widget):
   pageIndex = 0
-  #page = Image(source= './temp/comic/' + pages[pageIndex])
 
+  # Logic for binding page size to window taken from https://stackoverflow.com/questions/30207707/how-to-fill-canvas-with-an-image-in-kivy
   def __init__(self, **kwargs):
     super(Comic, self).__init__(**kwargs)
     with self.canvas:
       self.bg = Image(source = path + pages[self.pageIndex], pos=self.pos, size=self.size)
 
+    self.bind(pos=self.update_bg)
+    self.bind(size=self.update_bg)
+
+  def update_bg(self, *args):
+    self.bg.pos = self.pos
+    self.bg.size = self.size
+
   def update(self):
-    page = Image(source= './temp/comic/' + pages[self.pageIndex])
+    self.canvas.clear()
+    with self.canvas:
+      self.bg = Image(source = path + pages[self.pageIndex], pos=self.pos, size=self.size)
 
   def page_turn(self, touch):
     if touch.x < self.width / 3:
-      if self.pageIndex > 0:
-        self.pageIndex -= 1
+      if pageIndex > 0:
+        pageIndex -= 1
     if touch.x > self.width - self.width / 3:
-      if self.pageIndex < len(pages) - 1:
-        self.pageIndex += 1
+      if pageIndex < len(pages) - 1:
+        pageIndex += 1
     update()
 
   def build(self):
@@ -62,7 +73,8 @@ class Comic(Widget):
 class Gui(App):
 
   def build(self):
-    return Comic()
+    root = Comic()
+    return root
 
 if __name__ == '__main__':
 
